ea90cc15bc4b29e7cbd5d550e211c4106adfac2c
ggibm <2306824635@qq.com>
2019-08-28 10:28:57 +0800

    1

---
+++ src/main/java/com/nowcoder/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.nowcoder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/15.
+ */
+@Controller
+public class TestController {
+
+
+    @RequestMapping(path = {"/test"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(Model model) {
+        List<String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        model.addAttribute("model", list);
+        return "test";
+    }
+
+
+}
